b5d82e6cf15b1d71e6043d543ad0791d5f4c8e30
Simon Meißner <simon.meissner@free-now.com>
2022-12-01 16:50:45 +0100

    Day1 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,30 @@
 fun main() {
+    //part1
     val elfs = readFile("inputDay1").split("\n\n").map { Elf(it) }
-    val answer = getFattestElf(elfs)?.calories
+    val answer = getFattestElf(elfs).calories
     println("The most calories are $answer")
+
+    //part2
+    val answerPart2 = getTopThreeElfs(elfs).sumOf { it.calories }
+    println("The most calories are $answerPart2")
 }
 
-fun getFattestElf(elfs: List<Elf>) = elfs.maxByOrNull { it.calories }
+fun getFattestElf(elfs: List<Elf>) = elfs.maxBy { it.calories }
 
 class Elf(private val cal: String) {
     val calories = calculateCalories()
     fun calculateCalories(): Int {
         return cal.split("\n").sumOf { it.toInt() }
     }
+}
+
+fun getTopThreeElfs(elfs: List<Elf>): List<Elf>{
+    val topThree = emptyList<Elf>().toMutableList()
+    val elfsMutable = elfs.toMutableList()
+    repeat(3){
+        val elf = getFattestElf(elfsMutable)
+        topThree.add(elf)
+        elfsMutable.remove(elf)
+    }
+    return topThree.toList()
 }
