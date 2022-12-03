9f713c909d0d330da9730c6686308eee668d70d2
Simon Meißner <simon.meissner@free-now.com>
2022-12-03 05:22:44 +0100

    Day2 part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,12 +1,17 @@
 import GameResult.*
 
-
 fun main() {
+    // first
     val games = readInput("inputDay2").map { it.split(" ") }.map {
         Pair(Move.translateMove(it[1]), Move.translateMove(it[0]))
     }
-    print(calculatePoints(games))
+    println(calculatePoints(games))
 
+    val games2 = readInput("inputDay2").map { it.split(" ") }.map {
+        val enemyMove = Move.translateMove(it[0])
+        Pair(Move.translateMoveSecondRuleSet(enemyMove, it[1]), enemyMove)
+    }
+    println(calculatePoints(games2))
 }
 
 fun calculatePoints(games: List<Pair<Move, Move>>): Int {
@@ -16,13 +21,24 @@ fun calculatePoints(games: List<Pair<Move, Move>>): Int {
 val winMatrix = arrayOf(
     arrayOf(DRA, LOS, WIN), //rock
     arrayOf(WIN, DRA, LOS), //paper
-    arrayOf(LOS, WIN, DRA) //ccisor
+    arrayOf(LOS, WIN, DRA) //cisor
 )
 
 enum class GameResult(val points: Int) {
     WIN(6),
     DRA(3),
-    LOS(0)
+    LOS(0);
+
+    companion object {
+        fun fromMove(encryptedMove: String): GameResult {
+            return when (encryptedMove) {
+                "X" -> LOS
+                "Y" -> DRA
+                "Z" -> WIN
+                else -> throw IllegalArgumentException()
+            }
+        }
+    }
 }
 
 enum class Move(val index: Int, val points: Int) {
@@ -37,6 +53,14 @@ enum class Move(val index: Int, val points: Int) {
                 "B", "Y" -> PAPER
                 "C", "Z" -> CISOR
                 else -> throw IllegalArgumentException()
+            }
+        }
+
+        fun translateMoveSecondRuleSet(enemyMove: Move, encryptedMove: String): Move {
+            return when (GameResult.fromMove(encryptedMove)) {
+                LOS -> Move.values()[(winMatrix[enemyMove.index].indexOf(WIN))]
+                DRA -> Move.values()[(winMatrix[enemyMove.index].indexOf(DRA))]
+                WIN -> Move.values()[winMatrix[enemyMove.index].indexOf(LOS)]
             }
         }
     }
