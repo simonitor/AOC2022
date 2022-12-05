269c5f14aab2acb0d421c5ca84c756ab090cd4c3
Simon Meißner <simon.meissner@free-now.com>
2022-12-05 03:19:28 +0100

    Day3 refactor use destructuring

--- src/Day03.kt
+++ src/Day03.kt
@@ -11,13 +11,14 @@ fun findEquals(rucksacks: List<String>): Int {
     return rucksacks.map {
         it.substring(0, (it.length / 2)) to it.substring(it.length / 2, it.length)
     }.sumOf {
-        normalizeCharValue(it.first.toSet().intersect(it.second.toSet()).random())
+        normalizeCharValue((it.first.toSet() intersect it.second.toSet()).random())
     }
 }
 
 fun findBadge(rucksacks: List<String>): Int {
-    return rucksacks.windowed(3, 3).sumOf {
-        normalizeCharValue(it[0].toSet().intersect(it[1].toSet().intersect(it[2].toSet())).random())
+    return rucksacks.chunked(3).sumOf {
+        val (a, b, c) = it
+        normalizeCharValue((a.toSet() intersect b.toSet() intersect c.toSet()).random())
     }
 }
 
